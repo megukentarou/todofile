f0e8d9f01f59c8d7df26173f398e66cfd0f5f417
megukentarou <megukentarou@gmail.com>
2020-11-08 23:04:25 +0900

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,8 +14,8 @@
 
                 フレデリック・アミエル
                 </div>
-                
-                <!-- <img src="https://pixabay.com/get/53e0d2444b50a414f6d1867dda353678153bd9e65354784c_1920.jpg" width="100%" height="100%" alt="画像"> -->
+                <img src="https://pixabay.com/get/53e0d2444b50a414f6d1867dda353678153bd6e15058744a_1920.jpg" width="100%" height="100%" alt="ホーム画面の画像">
+              
                 
                 <div class="card-body">
                     <h5 class="card-text">アプリの説明</h5>
