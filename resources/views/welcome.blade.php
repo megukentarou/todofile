47f0b780c90002812f46353adc27ef57b5e1f4c7
megukentarou <megukentarou@gmail.com>
2020-11-08 23:24:28 +0900

    welcome ページ変更

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,36 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>Laravel</title>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">
+                <h6>心が変われば行動が変わる</h6><br>
+                <h6>行動が変われば習慣が変わる</h6><br>
+                <h5>習慣が変われば人格が変わる</h5><br>
+                <h4>人格が変われば運命が変わる</h4><br>
+                <h3>運命が変われば人生が変わる</h3><br>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                フレデリック・アミエル
                 </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <img src="https://pixabay.com/get/53e0d2444b50a414f6d1867dda353678153bd6e15058744a_1920.jpg" width="100%" height="100%" alt="ホーム画面の画像">
+              
+                
+                <div class="card-body">
+                    <h5 class="card-text">アプリの説明</h5>
+                    
+                    <p> 習慣にしていきたいToDoリストを好きな様に投稿出来る様にしています。出来るだけ詳細に残す事で、継続性を生み出して、最終的には習慣化出来る事で自身のライフプランをもっと良くしていける様になって欲しいと思っています。
+                    また全体にシェア（宣言）する事で、ある程度やらざるを得ない環境を作っていきましょう。
+                    他の人の投稿も見れますので、良かったなと思う内容はお互いどんどん取り入れていきましょう。</p>
+                    
+                    <div class="aaa">
+                    <a class="btn btn-success btn-lg" href="{{ route('words.index')}}" class="card-link"><h5>投稿一覧を見る</h5><h6>(ログインがまだの方はログインページに移ります)</h6></a>
+                    
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+</div>
+@endsection
